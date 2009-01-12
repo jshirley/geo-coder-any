7a420b4aca8404c10147dbc3f205a1ef40e8245e
Devin <devin.austin@gmail.com>
2009-01-11 21:37:51 -0700

    Worked on normalizing all data from Yahoo. Still getting an error saying there's an odd number of hash elements, haven't fixed that yet.

--- lib/Geo/Coder/Any/Yahoo.pm
+++ lib/Geo/Coder/Any/Yahoo.pm
@@ -12,8 +12,8 @@ new must be overridden. Geo::Coder::Yahoo assumes $class.
 =cut
 
 sub new {
-    my $class = shift;
-    my %args = @_;
+    my ( $class, %args ) = @_;
+    warn "Class: $class, Args: " . $args{appid};
     bless { $class, appid => $args{appid} };
 } 
 
@@ -27,6 +27,14 @@ sub process {
     }
 }
 
+=head1 _normalize($in)
+
+Normalize data returned from Geo::Coder::Yahoo for use with Geo::Coder::Any.
+Much less finer grain than Geo::Coder::Google, so some hacking must
+be done to populate all the proper hash keys
+
+=cut
+
 sub _normalize {
     my ( $self, $in ) = @_;
 
@@ -35,19 +43,35 @@ sub _normalize {
     my $locality = $aa->{SubAdministrativeArea}->{Locality} || 
                    $aa->{Locality};
     my $rs = {
-        'latitude'  => $in->{Point}->{coordinates}->[0],
-        'longitude' => $in->{Point}->{coordinates}->[1],
-
+        'latitude'  => $in->{country},
+        'longitude' => $in->{longitude},
         'address'             => $in->{address},
-        'thoroughfare'        => $locality->{Thoroughfare}->{ThoroughfareName},
-        'locality'            => $locality->{LocalityName},
-        'administrative_area' => $aa->{AdministrativeAreaName} || '',
-        'sub_administrative_area' => $aa->{SubAdministrativeArea}->{SubAdministrativeAreaName} || '',
-        'country' => $in->{AddressDetails}->{Country}->{CountryNameCode},
+       # 'thoroughfare'        => $locality->{Thoroughfare}->{ThoroughfareName},
+       # 'locality'            => $locality->{LocalityName},
+       # 'administrative_area' => $aa->{AdministrativeAreaName} || '',
+       # 'sub_administrative_area' => $aa->{SubAdministrativeArea}->{SubAdministrativeAreaName} || '',
+        'country' => $in->{country},
     };
 
     return $rs;
 }
 
+
+=head1 example Yahoo! data
+
+This is what Geo::Coder::Yahoo returns.  Much coarser grain than Geo::Coder::Yahoo.
+
+    {
+     'country' => 'US',
+     'longitude' => '-118.3387',
+     'state' => 'CA',
+     'zip' => '90028',
+     'city' => 'LOS ANGELES',
+     'latitude' => '34.1016',
+     'warning' => 'The exact location could not be found, here is the closest match: Hollywood Blvd At N Highland Ave, Los Angeles, CA 90028',
+     'address' => 'HOLLYWOOD BLVD AT N HIGHLAND AVE',
+     'precision' => 'address'
+     }
+=cut
 1;
 
